68c0c3971eda232f312e4dfabb63d8a1b34a7815
mobydickens <steadmegan@gmail.com>
2019-01-01 19:31:25 -0700

    Learning Jest

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,8 @@
+describe (`the question list`, () => {
+  beforeEach(() => {
+    console.log('before each!');
+  })
+  it (`should display a list of items`, () => {
+    expect(40 + 2).toEqual(42);
+  })
+})
